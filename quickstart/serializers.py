c01a9a274f489ba4a2c4c1d14482f08cbee86370
Akhil Mathew <akhilmakm@gmail.com>
2017-05-07 17:47:23 +0530

    Added serializer

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -3,3 +3,17 @@ Created on May 7, 2017
 
 @author: Akhil Mathew
 '''
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        fields = ('url', 'username', 'email', 'groups')
+        
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        fields = ('url', 'name')        
